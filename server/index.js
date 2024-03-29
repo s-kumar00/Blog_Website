071fb60809616f09e741fa0c0820f2cc77ff9604
s-kumar00 <sikadarsahilkumar@gmail.com>
2024-03-29 17:18:31 +0530

    MongoDb and user Schema

--- server/index.js
+++ server/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
-
+const cors = require("cors");
+const mongoose = require('mongoose');
 const app = express();
+app.use(cors());
+app.use(express.json());
+require("dotenv").config()
 
-app.listen(3000, () => {
-    console.log('server is running on port 3000!!');
-})
+// let server;
+
+const start = async () => {
+  try {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}...!`);
+    });
+
+    const database_connect = await mongoose.connect(process.env.MONGODB_URI);
+    if (database_connect) {
+      console.log("Database connected successfully...!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
