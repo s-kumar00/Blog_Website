1068e6343b6bffebd05e3c0bfdf90fffb692edc6
s-kumar00 <sikadarsahilkumar@gmail.com>
2024-04-14 23:39:38 +0530

    changed unsplash live image in contact page with query "map"

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,33 +1,7 @@
-import React, { useEffect } from "react";
-import axios from "axios";
-import { useState } from "react";
+import React from "react";
 
 const About = () => {
-  const [image, setImage] = useState();
-
-  useEffect(() => {
-    const getImage = async () => {
-      const random_number = Math.floor(Math.random() * 1000);
-      const query = "nature";
-      const response = await axios.get(
-        `https://source.unsplash.com/random/${random_number}?${query}`
-      );
-      setImage(response.request.responseURL);
-    };
-    getImage();
-  }, []);
-
-  return (
-    <div className="h-screen relative">
-      <div
-        className="absolute inset-0 bg-cover bg-no-repeat opacity-50"
-        style={{ backgroundImage: `url(${image})` }}
-      />
-      <div className="relative font-bold text-2xl text-black dark:text-white">
-        About
-      </div>
-    </div>
-  );
+  return <div className="h-screen">About</div>;
 };
 
 export default About;
